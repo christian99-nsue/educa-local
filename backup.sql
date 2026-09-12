5cd397b8c3113aeeb611bc231a2a9d6131e52d9d
Christian Nsue <christiannsue00@gmail.com>
2026-09-12 16:43:40 +0100

    Modelo responsive completado y añadido arreglos

--- backup.sql
+++ backup.sql
@@ -3,6 +3,7 @@
 -- Host: localhost    Database: edulocal
 -- ------------------------------------------------------
 -- Server version	8.0.35
+select count(*) from usuarios;
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
